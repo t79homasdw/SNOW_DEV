06d5d00becf4004e33082d9fbe7667c9c45b8f6b
Dan Thomas <68958256+t79homasdw@users.noreply.github.com>
2024-10-29 12:37:51 -0400

    Update Web_Request2.ps1

--- Web_Request2.ps1
+++ Web_Request2.ps1
@@ -10,11 +10,12 @@ Remove-Item -Path "C:\Certreq\*" -Recurse -Force -ErrorAction SilentlyContinue
 copy-item -path "C:\Cert_Config\root.crt" -Destination C:\Certreq -Force
 $path = "C:\Certreq\requestconfig.inf"
 
-
+<#
 $sub = "www.archgroup.com"
 $fn = "Wildfire"
 $sans_input = "www.arch.com,www.buttler.com,www.fieldtype.com,www.beanbag.com"
 $email_chain = "amis-domainhybridteam_corp@archgroup.com,dthomas@archgroup.com"
+#>
 
 $subject = $sub -replace(' ')
 $fname = $fn -replace(' ')
