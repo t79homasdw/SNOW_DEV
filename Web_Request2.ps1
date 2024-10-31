ca611080db95e337db324c9a884cab1314799e0a
Dan Thomas <68958256+t79homasdw@users.noreply.github.com>
2024-10-31 01:23:16 -0400

    Update Web_Request2.ps1

--- Web_Request2.ps1
+++ Web_Request2.ps1
@@ -112,11 +112,11 @@ $File = "C:\Cert_Config\Email_Passwd.txt"
 $email_pass = Get-Content $File
 $pass = $email_pass | ConvertTo-SecureString
 $cred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$pass
-
+$e_sub = "New Certificate" + $f_name2
 
 $EmailTo = $email
 $EmailFrom = $User
-$Subject = $f_subject
+$Subject = $e_sub
 $body= "Congratulations Here is the certificate that you requested."
 $smtpserver = "smtp.mailersend.net"
 $filenameAndPath = $Target
